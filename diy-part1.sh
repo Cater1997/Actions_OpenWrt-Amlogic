dcd41d0afe213e8a933332c6477814aecac4ca56
Cater1997 <92653925+Cater1997@users.noreply.github.com>
2026-02-13 10:11:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,19 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
+# 取消与瑞莎 E52c 无关的软件包
+sed -i 's/CONFIG_PACKAGE_arm-trusted-firmware-microchipsw=y/# CONFIG_PACKAGE_arm-trusted-firmware-microchipsw is not set/' .config
+sed -i 's/CONFIG_PACKAGE_audit=y/# CONFIG_PACKAGE_audit is not set/' .config
+sed -i 's/CONFIG_PACKAGE_auditd=y/# CONFIG_PACKAGE_auditd is not set/' .config
+sed -i 's/CONFIG_PACKAGE_autosamba=y/# CONFIG_PACKAGE_autosamba is not set/' .config
+sed -i 's/CONFIG_BUSYBOX_CONFIG_PAM=y/# CONFIG_BUSYBOX_CONFIG_PAM is not set/' .config
+sed -i 's/CONFIG_BUSYBOX_CONFIG_RPC=y/# CONFIG_BUSYBOX_CONFIG_RPC is not set/' .config
+sed -i 's/CONFIG_PACKAGE_kexec-tools=y/# CONFIG_PACKAGE_kexec-tools is not set/' .config
+sed -i 's/CONFIG_PACKAGE_lldpd=y/# CONFIG_PACKAGE_lldpd is not set/' .config
+sed -i 's/CONFIG_PACKAGE_onionshare-cli=y/# CONFIG_PACKAGE_onionshare-cli is not set/' .config
+sed -i 's/CONFIG_PACKAGE_pcat-manager=y/# CONFIG_PACKAGE_pcat-manager is not set/' .config
+sed -i 's/CONFIG_PACKAGE_policycoreutils=y/# CONFIG_PACKAGE_policycoreutils is not set/' .config
+
 ## 常用OpenWrt软件包源码合集，同步上游更新！
 ## 通用版luci适合18.06与19.07
 # echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
