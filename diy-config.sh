f9ccc19446589627a0c352d897d9528e847e9086
Cater1997 <92653925+Cater1997@users.noreply.github.com>
2026-02-13 14:59:09 +0800

    Update diy-config.sh

--- diy-config.sh
+++ diy-config.sh
@@ -1,4 +1,4 @@
-# 编辑默认LAN口IP
+# 编辑默认 LAN 口 IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 编辑默认固件名称
@@ -7,7 +7,7 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # 编辑默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 取消与瑞莎E52c无关的软件包
+# 取消与瑞莎 E52c 无关的软件包
 sed -i 's/CONFIG_PACKAGE_arm-trusted-firmware-microchipsw=y/# CONFIG_PACKAGE_arm-trusted-firmware-microchipsw is not set/' .config
 sed -i 's/CONFIG_PACKAGE_audit=y/# CONFIG_PACKAGE_audit is not set/' .config
 sed -i 's/CONFIG_PACKAGE_auditd=y/# CONFIG_PACKAGE_auditd is not set/' .config
@@ -20,6 +20,6 @@ sed -i 's/CONFIG_PACKAGE_onionshare-cli=y/# CONFIG_PACKAGE_onionshare-cli is not
 sed -i 's/CONFIG_PACKAGE_pcat-manager=y/# CONFIG_PACKAGE_pcat-manager is not set/' .config
 sed -i 's/CONFIG_PACKAGE_policycoreutils=y/# CONFIG_PACKAGE_policycoreutils is not set/' .config
 
-# 修改luci-app-amlogic配置
+# 修改 luci-app-amlogic 配置
 sed -i 's#https://github.com/breakings/OpenWrt#https://github.com/ophub/kernel#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
 sed -i 's#opt/kernel#kernel#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
